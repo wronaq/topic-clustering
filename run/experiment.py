3dba820384a77ec28bf175ed223b1ed110283654
Łukasz Woronkiewicz <woronkiewicz@gmail.com>
2021-04-16 17:50:05 +0200

    feat: add dimensionality reduction with umap

--- run/experiment.py
+++ run/experiment.py
@@ -6,39 +6,51 @@ import torch
 
 def run_experiment(experiment_config, save_embeddings=True):
 
-    # load data
-    data_module_ = importlib.import_module("topic_clustering.data_loader")
-    dataloader_class_ = getattr(data_module_, experiment_config["dataloader"])
-    dataloader_args_ = experiment_config.get("dataloader_args", {})
-    dataset = dataloader_class_(**dataloader_args_)
+    try:
+        embeddings = torch.load("sentence_embeddings.pt")
+    except:
+        # load data
+        data_module_ = importlib.import_module("topic_clustering.data_loader")
+        dataloader_class_ = getattr(data_module_, experiment_config["dataloader"])
+        dataloader_args_ = experiment_config.get("dataloader_args", {})
+        dataset = dataloader_class_(**dataloader_args_)
 
-    # generate sentence embeddings
-    model_module_ = importlib.import_module("sentence_transformers")
-    model_class_ = getattr(model_module_, "SentenceTransformer")
-    model_args_ = {
-        k: v
-        for k, v in experiment_config.get(
-            "model_args",
-            {
-                "model_name_or_path": "sentence-transformers/xlm-r-100langs-bert-base-nli-stsb-mean-tokens"
-            },
-        ).items()
-        if k in ["model_name_or_path", "device"]
-    }
-    encode_args_ = {
-        k: v
-        for k, v in experiment_config.get("model_args", {}).items()
-        if k not in ["model_name_or_path", "max_seq_length"]
-    }
-    max_seq_length_ = experiment_config.get("model_args", {}).get("max_seq_length", {})
-    model = model_class_(**model_args_)
-    model.max_seq_length = max_seq_length_
-    embeddings = model.encode(
-        dataset.load_data(), show_progress_bar=True, **encode_args_
-    )
+        # generate sentence embeddings
+        model_module_ = importlib.import_module("sentence_transformers")
+        model_class_ = getattr(model_module_, "SentenceTransformer")
+        model_args_ = {
+            k: v
+            for k, v in experiment_config.get(
+                "model_args",
+                {
+                    "model_name_or_path": "sentence-transformers/xlm-r-100langs-bert-base-nli-stsb-mean-tokens"
+                },
+            ).items()
+            if k in ["model_name_or_path", "device"]
+        }
+        encode_args_ = {
+            k: v
+            for k, v in experiment_config.get("model_args", {}).items()
+            if k not in ["model_name_or_path", "max_seq_length"]
+        }
+        max_seq_length_ = experiment_config.get("model_args", {}).get(
+            "max_seq_length", {}
+        )
+        model = model_class_(**model_args_)
+        model.max_seq_length = max_seq_length_
+        embeddings = model.encode(
+            dataset.load_data(), show_progress_bar=True, **encode_args_
+        )
 
-    if save_embeddings:
-        torch.save(embeddings, "sentence_embeddings.pt")
+        if save_embeddings:
+            torch.save(embeddings, "sentence_embeddings.pt")
+
+    # dimensionality reduction
+    umap_module_ = importlib.import_module("umap")
+    umap_class_ = getattr(umap_module_, "UMAP")
+    umap_args_ = experiment_config.get("umap_args", {})
+    umap = umap_class_(**umap_args_)
+    low_dim_embeddings = umap.fit_transform(embeddings)
 
 
 def _parse_args():
