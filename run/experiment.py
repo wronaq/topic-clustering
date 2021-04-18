c33f76e6e69a1db26797b3726cedd5740f7bf958
Łukasz Woronkiewicz <woronkiewicz@gmail.com>
2021-04-18 17:57:20 +0200

    feat: add clustering and class-based tfidf

--- run/experiment.py
+++ run/experiment.py
@@ -2,13 +2,17 @@ import argparse
 import importlib
 import json
 import torch
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 
-def run_experiment(experiment_config, save_embeddings=True):
+def run_experiment(experiment_config, n_topics, n_words, save_embeddings=True):
 
     try:
         embeddings = torch.load("sentence_embeddings.pt")
     except:
+        logging.info("Creating sentence embeddings")
         # load data
         data_module_ = importlib.import_module("topic_clustering.data_loader")
         dataloader_class_ = getattr(data_module_, experiment_config["dataloader"])
@@ -39,18 +43,37 @@ def run_experiment(experiment_config, save_embeddings=True):
         model = model_class_(**model_args_)
         model.max_seq_length = max_seq_length_
         embeddings = model.encode(
-            dataset.load_data(), show_progress_bar=True, **encode_args_
+            dataset.load_data()[:32], show_progress_bar=True, **encode_args_
         )
 
         if save_embeddings:
             torch.save(embeddings, "sentence_embeddings.pt")
+    else:
+        logging.info("Sentence embeddings loaded from file")
 
     # dimensionality reduction
+    logging.info("Dimensionality reduction")
     umap_module_ = importlib.import_module("umap")
     umap_class_ = getattr(umap_module_, "UMAP")
     umap_args_ = experiment_config.get("umap_args", {})
     umap = umap_class_(**umap_args_)
     low_dim_embeddings = umap.fit_transform(embeddings)
+
+    # clustering
+    logging.info("Clustering")
+    hdbscan_module_ = importlib.import_module("hdbscan")
+    hdbscan_class_ = getattr(hdbscan_module_, "HDBSCAN")
+    hdbscan_args_ = experiment_config.get("hdbscan_args", {})
+    hdbscan = hdbscan_class_(**hdbscan_args_)
+    clusters = hdbscan.fit(low_dim_embeddings)
+
+    # class-based TF-IDF
+    logging.info("Calculate class-based TF-IDF")
+    topic_module_ = importlib.import_module("topic_clustering.topics")
+    topic_class_ = getattr(topic_module_, "Topic")
+    topic_args_ = experiment_config.get("topic_args", {})
+    topic = topic_class_(dataset, clusters, n_topics, **topic_args_)
+    topic.top_n_words(n_words)
 
 
 def _parse_args():
@@ -59,6 +82,16 @@ def _parse_args():
         "experiment_config",
         type=str,
         help='Path to experiment JSON like: \'{"dataloader": "DataLoader", "model_args": {"model_name_or_path": "sentence-transformers/xlm-r-100langs-bert-base-nli-stsb-mean-tokens"}}\'',
+    )
+    parser.add_argument(
+        "n_topics",
+        type=int,
+        help="Number of topics to find. If zero then no reduction is made.",
+    )
+    parser.add_argument(
+        "n_words",
+        type=int,
+        help="Top n words that describe each topic.",
     )
     parser.add_argument(
         "--save_embeddings",
@@ -75,7 +108,7 @@ def main():
     with open(args.experiment_config, "r") as f:
         config = f.read()
     experiment_config = json.loads(config)
-    run_experiment(experiment_config, args.save_embeddings)
+    run_experiment(experiment_config, args.n_topics, args.n_words, args.save_embeddings)
 
 
 if __name__ == "__main__":
--- tasks/cluser_data.sh
+++ tasks/cluser_data.sh
@@ -4,7 +4,9 @@ if [ $# -lt 1 ]; then
     echo "Expecting "
     echo "Expecting: "
     echo "1) path to config file,"
-    echo "2) flag --save_embeddings (optional)"
+    echo "2) number of topics to find. If zero then no reduction is made,"
+    echo "3) top n words to describe each topic,"
+    echo "4) flag --save_embeddings (optional)"
     exit 1
 fi
 
